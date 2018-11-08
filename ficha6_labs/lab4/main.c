2c8034b04098769d99fbf9551ff1f5c2ab7401b4
diogogomes21 <mandiogo99@hotmail.com>
2018-11-08 13:06:46 +0000

    08/11/2018

--- ficha6_labs/lab4/main.c
+++ ficha6_labs/lab4/main.c
@@ -13,6 +13,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <assert.h>
+#include <arpa/inet.h>
 
 #include "debug.h"
 #include "memory.h"
@@ -22,12 +23,32 @@ int main(int argc, char *argv[]){
 	(void)argc; (void)argv;
 
     /* a) Criar um socket UDP / IPv4 */
+		int sock_udp_ipv4;
+
+		sock_udp_ipv4 = socket(AF_INET,SOCK_DGRAM,0);
+		if (sock_udp_ipv4==-1) {
+			fprintf(stderr, "Cannot create socket IPv4 / UDP: %s\n", strerror(errno));
+			exit(1);
+		}
 
     /* b) Criar um socket UDP / IPv6 */
+		int sock_udp_ipv6;
+
+		sock_udp_ipv6 = socket(AF_INET,SOCK_DGRAM,0);
+		if (sock_udp_ipv4==-1) {
+			fprintf(stderr, "Cannot create socket IPv4 / UDP: %s\n", strerror(errno));
+			exit(1);
+		}
 
     /* c) Mostrar descritores dos sockets */
 
+		printf("sock_udp_ipv4=%d\n", sock_udp_ipv4);
+		printf("sock_udp_ipv6=%d\n", sock_udp_ipv6);
+
     /* d) Fechar os descritores dos sockets */
+
+		close(sock_udp_ipv4);
+		close(sock_udp_ipv6);
 
 	return 0;
 }
